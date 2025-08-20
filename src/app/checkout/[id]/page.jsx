2a09ca4731d27259552f911dbe107d5839f2e1ee
MMunim <mmunim9.9.01@gmail.com>
2025-08-20 11:30:07 +0600

    make checkout page

---
+++ src/app/checkout/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default async function checkoutPage({params}) {
+  const p = await params;
+  const res = await fetch(`http://localhost:3000/api/service/${p.id}`);
+  const data = await res.json();
+  return <div>{JSON.stringify(data)}</div>;
+}
+
--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,36 +1,52 @@
-import dbConnect, { collectionNameObj } from "@/lib/dbConnect";
-import { ObjectId } from "mongodb";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default async function serviceDetailPage({ params }) {
   const p = await params;
-  const servicesCollection = dbConnect(collectionNameObj.servicesCollection);
-  const data = await servicesCollection.findOne({ _id: new ObjectId(p.id) });
+  const res = await fetch(`http://localhost:3000/api/service/${p.id}`)
+  const data = await res.json();
   return (
     <div className="w-11/12 mx-auto space-y-4">
       <section className="flex justify-center">
         <figure className="relative">
-            <Image src={"/assets/images/checkout/checkout.png"} width={1200} height={300} alt="banner" className="mx-auto"></Image>
+          <Image
+            src={"/assets/images/checkout/checkout.png"}
+            width={1200}
+            height={300}
+            alt="banner"
+            className="mx-auto"
+          ></Image>
 
-            <div className="transparent-layer overlay-bg absolute w-full h-full rounded top-0">
-                <div className="w-full h-full flex items-center ml-24">
-                    <div>
-                        <h1 className="text-white text-4xl font-bold">Service Details</h1>
-                    </div>
-                </div>
+          <div className="transparent-layer overlay-bg absolute w-full h-full rounded top-0">
+            <div className="w-full h-full flex items-center ml-24">
+              <div>
+                <h1 className="text-white text-4xl font-bold">
+                  Service Details
+                </h1>
+              </div>
             </div>
+          </div>
         </figure>
       </section>
 
-      <section className="w-8/12">
-        <Image src={data.img} width={800} height={280} alt={data.title}></Image>
-        <h1 className="font-bold text-4xl mt-6">{data.title}</h1>
-        <p>{data.description}</p>
-      </section>
-      <section className="w-4/12">
-        
-      </section>
+      <div className="flex gap-4">
+        <section className="w-9/12">
+          <Image
+            src={data.img}
+            width={1000}
+            height={280}
+            alt={data.title}
+          ></Image>
+          <h1 className="font-bold text-4xl mt-6">{data.title}</h1>
+          <p>{data.description}</p>
+        </section>
+        <section className="w-3/12">
+        <Link href={`/checkout/${data._id}`}>
+          <button className="w-full bg-amber-600 text-white py-3 text-xl font-bold cursor-pointer">Checkout</button></Link>
+          <p className="text-center mt-4 text-2xl font-bold">Price: ${data.price}</p>
+        </section>
+      </div>
     </div>
   );
 }
